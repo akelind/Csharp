34749288c169f42238d4a339f5b64feea8321ebc
Ake Lind <ald.devops@gmail.com>
2020-06-12 13:59:23 +0200

    Added AddingNumbers in jason file

---
+++ ConsoleApplications/AddingNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstSetupGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding numbers");
+        }
+    }
+}
