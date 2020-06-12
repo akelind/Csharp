c6da36ceeffdb7d1950aa1432c1835f8ebdcd353
Ake Lind <ald.devops@gmail.com>
2020-06-12 16:46:28 +0200

    adding numbers in diffrent ways of arrays

--- ConsoleApplications/AddingNumbers/Program.cs
+++ ConsoleApplications/AddingNumbers/Program.cs
@@ -6,7 +6,36 @@ namespace FirstSetupGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Adding numbers");
+            /*
+            double Fx= 33.33;
+            double Fy= 33.33;
+            var rd=Fx+Fy;
+           Console.WriteLine("Som of all float numbers are: " + rd);
+            */
+
+            /*
+            var numbers = new double[3];
+            numbers [0] = 33.66;
+            numbers [1] = 33.66;
+            numbers [2] = 33.66;
+
+            var rda= numbers[0];
+            rda = rda + numbers[1];
+            rda = rda + numbers[2];
+
+            System.Console.WriteLine(rda);
+            */
+
+            
+            var arrnum = new[]{33.99,33.99,33.99};
+            var rarrnum = 0.0;
+            foreach (var ddn in arrnum)
+            {
+                 rarrnum +=ddn;   
+            }
+            System.Console.WriteLine(rarrnum);
+            
+            
         }
     }
 }
