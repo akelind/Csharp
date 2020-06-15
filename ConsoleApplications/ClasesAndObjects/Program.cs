c9560a79b8fb66e268a496da34c1883f6206ea2f
Ake Lind <ald.devops@gmail.com>
2020-06-15 15:02:04 +0200

    Added list of numbers and sums of numbers in the list

--- ConsoleApplications/ClasesAndObjects/Program.cs
+++ ConsoleApplications/ClasesAndObjects/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ClasesAndObjects
 {
     
@@ -7,15 +7,40 @@ namespace ClasesAndObjects
     {
         static void Main(string[] args)
         {
-            Memory.Memorytest();
+            //Memory.Memorytest();
+            Grades.CalcAvragegrade();
         }
         
     }
-    public class Memory
+    public class Grades
+    {
+            public static void CalcAvragegrade()
+            {
+                List<double> dnumbers = new List<double>();
+                dnumbers.Add(33.34);
+                dnumbers.Add(33.35);
+                dnumbers.Add(33.36);
+
+                double dsum = 0.0;    
+                foreach (var dnumber in dnumbers)
+                
+                {
+                    dsum += dnumber;    
+                    System.Console.WriteLine($"Numbers in the list:{dnumber}" );
+                    System.Console.WriteLine($"Sum of all numbers:{dsum} \n");
+                }
+                   //System.Console.WriteLine(); 
+
+            } 
+    }
+
+
+    /*public class Memory
     {
         public static void Memorytest()
             {
                 System.Console.WriteLine("I remeber how to make classes but what about objects, maybe of a car?");
             }
     }
+    */
 }
