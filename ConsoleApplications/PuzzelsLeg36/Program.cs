aa34e20026b320f55967490318348ef5b1700a66
Ake Lind <ald.devops@gmail.com>
2020-08-12 14:18:46 +0200

    Added checks of letters

--- ConsoleApplications/PuzzelsLeg36/Program.cs
+++ ConsoleApplications/PuzzelsLeg36/Program.cs
@@ -1,55 +1,36 @@
 ﻿using System;
 using System.IO;
 using System.Data;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+					
+public class Program
+{
+	public static void Main()
+	{
+		
+        StreamReader sr = new StreamReader("D:\\CSharp2020\\ConsoleApplications\\PuzzelsLeg36\\leq36.txt");
+        string data = sr.ReadToEnd();      
+        string[] words = data.Split(',');
 
-namespace PuzzelsLeg36
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Console.WriteLine("Hello World!");
-            String line;
-try
-{
-    //Pass the file path and file name to the StreamReader constructor
-    //StreamReader sr = new StreamReader("C:\\test\\Sample.txt");
-    StreamReader sr = new StreamReader("D:\\CSharp2020\\ConsoleApplications\\PuzzelsLeg36\\leq36.txt");
-     string data = sr.ReadToEnd();      
-      //sr.Close();
- 
-      string[] values = data.Split(',');
- 
-      for (int i = 0; i < values.Length; i++)
-      {
-        //MessageBox.Show(values[i]);
-        System.Console.WriteLine((values[i]));
-      }
-    
-    //Read the first line of text
-    line = sr.ReadLine();
-    //Continue to read until you reach end of file
-    while (line != null)
-    {
-        //write the lie to console window
-        Console.WriteLine(line);
-        //Read the next line
-        line = sr.ReadLine();
-    }
-    //close the file
-    sr.Close();
-    Console.ReadLine();
-}
-catch(Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
-}
-finally
-{
-    Console.WriteLine("Executing finally block.");
-}
-
-
-        }
-    }
+		Console.WriteLine(string.Join(", "+"\n", Get4PuzzlePieces(words)));
+        //Console.WriteLine("[{0}]"+"\n", string.Join(", ", Get4PuzzlePieces(words)));
+	}
+	
+	public static string[] Get4PuzzlePieces(string[] words)
+	{
+		Regex rgx = new Regex("[^a-zA-Z]");
+		List<string> _4PuzzlePiecesLetters = new List<string>();
+		for (int i = 0; i < words.Length; i++)
+		{
+			string word = rgx.Replace(words[i]+"\n", "");
+			if (!string.IsNullOrEmpty(word) && word.Length <=4 )
+			{
+				_4PuzzlePiecesLetters.Add(words[i]);
+			}
+			
+		}
+			
+		return _4PuzzlePiecesLetters.ToArray();
+	}
 }
